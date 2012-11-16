fd5f741f5ea4178002587403d6fba0d0491910ee
Mark Steve Samson <blazinggale@gmail.com>
2012-11-16 11:10:35 +0700

    Bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="Flask-StatHat",
-    version='0.0.1',
+    version='0.0.2',
     url='https://github.com/marksteve/flask-stathat',
     license='MIT',
     author="Mark Steve Samson",
