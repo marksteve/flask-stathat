0b1e7c646ceaedc40b9d63e49f4310a6a58f8ffe
Mark Steve Samson <hello@marksteve.com>
2014-07-31 12:46:37 +0800

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="Flask-StatHat",
-    version='0.1.0',
+    version='0.1.1',
     url='https://github.com/marksteve/flask-stathat',
     license='MIT',
     author="Mark Steve Samson",
